a7286a2d7b128b223dcd6421e730066d879832b8
Sagar <avhad.avhad@gmail.com>
2021-10-28 23:41:55 +0530

    runBlocking creates coroutine that block current thread.

--- src/main/kotlin/KotlinCoroutines2.kt
+++ src/main/kotlin/KotlinCoroutines2.kt
@@ -1,12 +1,12 @@
 import kotlinx.coroutines.*
 
-fun main(){ // Execute in main thread
-    println("Main starts: ${Thread.currentThread().name}")
-    GlobalScope.launch { //coroutine created on background thread
-        Thread.sleep(1000)
-        println("Fake work starts: ${Thread.currentThread().name}")
-        Thread.sleep(1000)
-        println("Fake work ends: ${Thread.currentThread().name}")
-    }
-    println("Main ends: ${Thread.currentThread().name}")
+fun main() = runBlocking{ //create coroutine that blocks main thread
+        println("Main starts: ${Thread.currentThread().name}")
+        GlobalScope.launch { //coroutine created on background thread T1
+            println("Fake work starts: ${Thread.currentThread().name}") //T1
+            delay(1000)//T1 free
+            println("Fake work ends: ${Thread.currentThread().name}")//T1 or other Thread
+        }
+        delay(2000)
+        println("Main ends: ${Thread.currentThread().name}")
 }
