9e4987792dc3ea04b79fc9f945820af1e7c3ef33
Sagar <avhad.avhad@gmail.com>
2021-10-28 23:48:15 +0530

    created custom suspend function

--- src/main/kotlin/KotlinCoroutines2.kt
+++ src/main/kotlin/KotlinCoroutines2.kt
@@ -1,12 +1,16 @@
 import kotlinx.coroutines.*
 
-fun main() = runBlocking{ //create coroutine that blocks main thread
-        println("Main starts: ${Thread.currentThread().name}")
-        GlobalScope.launch { //coroutine created on background thread T1
-            println("Fake work starts: ${Thread.currentThread().name}") //T1
-            delay(1000)//T1 free
-            println("Fake work ends: ${Thread.currentThread().name}")//T1 or other Thread
-        }
-        delay(2000)
-        println("Main ends: ${Thread.currentThread().name}")
+fun main() = runBlocking { //create coroutine that blocks main thread
+    println("Main starts: ${Thread.currentThread().name}")
+    GlobalScope.launch { //coroutine created on background thread T1
+        println("Fake work starts: ${Thread.currentThread().name}") //T1
+        mySuspendFunction(1000)//T1 free
+        println("Fake work ends: ${Thread.currentThread().name}")//T1 or other Thread
+    }
+    mySuspendFunction(2000)
+    println("Main ends: ${Thread.currentThread().name}")
+}
+
+private suspend fun mySuspendFunction(time: Long) {
+    delay(time)
 }
